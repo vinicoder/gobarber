722254a233f312198a7168bdd3392e4d4d64fafa
Vinicius de Moraes <vinicoder@gmail.com>
2020-03-15 22:55:46 -0300

    feature: available controller and appointments new features

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -30,7 +30,7 @@ class AppointmentController {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit,
       offset: (page - 1) * limit,
       include: [
